02e2284a1a1a746b62aaf812823713d98447c9c1
NajibHebrawi <NajibHebrawi@hotmail.com>
2022-02-21 16:47:23 +0100

    some deleted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,107 @@
+
+import { useState } from 'react'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 import logo from './logo.svg';
 import './App.css';
 
+
+import React, {useState } from 'react' 
+
+const App = () =>{
+  const [counter, setCounter] = useState(0)
+
+  const incrementCounter = () => {
+    setCounter(counter + 1)
+  }
+
+  return (
+    <div>
+      <button onClick={incrementCounter}> Increment</button>
+      {counter}
+      <p> HELLO CLASS</p>
+    </div>
+
+  )
+}
+
+export default App
+
+/*
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Hello 02363 class
+        </p>
+        <p>
+        Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -23,3 +113,4 @@ function App() {
 }
 
 export default App;
+*/
